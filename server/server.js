1d63c29f2b6c9f262a883f8032c3bc78d0c46bb3
Mariusz <mariusz.tripler@gmail.com>
2016-11-09 12:41:14 +0000

    AddPost /users/login route

--- server/server.js
+++ server/server.js
@@ -111,6 +111,19 @@ app.get('/users/me',authenticate,(req,res)=> {
     res.send(req.user);
 });
 
+//POST users/login/ 
+app.post('/users/login',(req,res)=> {
+    var body = _.pick(req.body,['email','password']);
+    User.findByCredentials(body.email,body.password).then((user)=> {
+       return user.generateAuthToken().then((token)=> {
+            res.header('x-auth',token).send(user);
+        })
+    }).catch((e)=> {
+        res.status(400).send();
+    });
+});
+
+
 const port = process.env.PORT || 3000;
 
 app.listen(port,() => {
