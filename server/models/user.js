b4c8e5680329f108de1e1cfdde18f8daefcbabd2
Mariusz <mariusz.tripler@gmail.com>
2016-11-09 17:38:07 +0000

    add tests for POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -70,7 +70,7 @@ UserSchema.statics.findByToken = function (token) {
    });
 };
 
-UserSchema.statics.findByCredentials = function (email,password) {
+UserSchema.statics.findByCredentials = function (email, password) {
     var User = this;
     return User.findOne({email}).then((user)=>{
         if(!user) {
